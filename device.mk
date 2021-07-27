8a3677accfcdcf857eb3b8573f8f084345ca4d51
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-27 18:59:50 +0700

    platina: add xiaomi parts
    use xiaomi parts from https://github.com/ArrowOS-Devices/android_device_xiaomi_sdm660-common/tree/arrow-11.0/parts

--- device.mk
+++ device.mk
@@ -351,6 +351,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \

033ceacd9673b6ce895ca53862820c1d10552843
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-08-28 19:59:29 -0300

    platina: Initial bringup AOSPK

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -19,13 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AOSPK stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-# PixelExperience Properties
+# AOSPK Properties
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+CUSTOM_MAINTAINER := Allan_dk
+TARGET_FACE_UNLOCK_SUPPORTED := true
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)

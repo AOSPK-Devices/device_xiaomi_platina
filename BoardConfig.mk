c5db77f7b751a4ed66cc8f4d315f6878e0975b6f
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-10-15 10:17:08 +0000

    platina: IGNORE NEVERALLOWS for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,7 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
